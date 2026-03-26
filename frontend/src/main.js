a938993da3e6464945d91003f0ffa1f6f6481908
romaxa <romaxa@MacBook-Air-romaxa.local>
2026-03-27 01:51:25 +0700

    добавление vue-router

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,6 +1,7 @@
 import { createApp } from 'vue'
 import './styles/style.css'
 import App from './App.vue'
+import router from './router'
 
 
-createApp(App).mount('#app')
+createApp(App).use(router).mount('#app')
